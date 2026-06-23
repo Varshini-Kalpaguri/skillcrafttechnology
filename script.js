471d942e6490fffd6d1a01bbf9cb546079777255
Varshini-Kalpaguri <kvarshinikalpaguri@gmail.com>
2026-06-23 12:15:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,107 +1,50 @@
-const quizData = [
-{
-question: "What does HTML stand for?",
-a: "Hyper Text Markup Language",
-b: "High Text Machine Language",
-c: "Hyper Transfer Markup Language",
-d: "Home Tool Markup Language",
-correct: "a"
-},
-{
-question: "Which language is used for styling web pages?",
-a: "HTML",
-b: "CSS",
-c: "Python",
-d: "Java",
-correct: "b"
-},
-{
-question: "Which language is used for web interactivity?",
-a: "JavaScript",
-b: "CSS",
-c: "HTML",
-d: "C++",
-correct: "a"
-},
-{
-question: "Which company developed JavaScript?",
-a: "Google",
-b: "Microsoft",
-c: "Netscape",
-d: "Apple",
-correct: "c"
-},
-{
-question: "Which tag is used to create a hyperlink?",
-a: "<link>",
-b: "<a>",
-c: "<href>",
-d: "<url>",
-correct: "b"
-}
-];
+function addTask() {
 
-const questionEl = document.getElementById("question");
-const answerEls = document.querySelectorAll(".answer");
-const a_text = document.getElementById("a_text");
-const b_text = document.getElementById("b_text");
-const c_text = document.getElementById("c_text");
-const d_text = document.getElementById("d_text");
-const submitBtn = document.getElementById("submit");
+    let taskInput = document.getElementById("taskInput");
+    let taskDate = document.getElementById("taskDate");
 
-let currentQuiz = 0;
-let score = 0;
-
-loadQuiz();
-
-function loadQuiz(){
-    deselectAnswers();
-
-    const currentData = quizData[currentQuiz];
-
-    questionEl.innerText = currentData.question;
-    a_text.innerText = currentData.a;
-    b_text.innerText = currentData.b;
-    c_text.innerText = currentData.c;
-    d_text.innerText = currentData.d;
-}
-
-function deselectAnswers(){
-    answerEls.forEach(answer => answer.checked = false);
-}
-
-function getSelected(){
-    let answer;
-
-    answerEls.forEach(answerEl => {
-        if(answerEl.checked){
-            answer = answerEl.id;
-        }
-    });
-
-    return answer;
-}
-
-submitBtn.addEventListener("click", () => {
-
-    const answer = getSelected();
-
-    if(answer){
-        if(answer === quizData[currentQuiz].correct){
-            score++;
-        }
-
-        currentQuiz++;
-
-        if(currentQuiz < quizData.length){
-            loadQuiz();
-        } else {
-            document.getElementById("quiz").innerHTML = `
-            <h2>You scored ${score}/${quizData.length}</h2>
-            <button onclick="location.reload()">
-                Restart Quiz
-            </button>
-            `;
-        }
+    if(taskInput.value === ""){
+        alert("Enter a task");
+        return;
     }
-});
+
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+        <div>
+            <strong>${taskInput.value}</strong><br>
+            <small>${taskDate.value}</small>
+        </div>
+
+        <div class="actions">
+            <button onclick="completeTask(this)">✓</button>
+            <button onclick="editTask(this)">Edit</button>
+            <button onclick="deleteTask(this)">Delete</button>
+        </div>
+    `;
+
+    document.getElementById("taskList").appendChild(li);
+
+    taskInput.value = "";
+    taskDate.value = "";
+}
+
+function completeTask(button){
+    button.parentElement.parentElement.classList.toggle("completed");
+}
+
+function deleteTask(button){
+    button.parentElement.parentElement.remove();
+}
+
+function editTask(button){
+
+    let taskText =
+    button.parentElement.previousElementSibling.querySelector("strong");
+
+    let newTask = prompt("Edit Task", taskText.innerText);
+
+    if(newTask){
+        taskText.innerText = newTask;
+    }
+}
